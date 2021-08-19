557b3c378fc0ef8e03a821a0a0af375008710a63
emircaglar <emircaglar2906@gmail.com>
2021-08-19 19:44:40 +0200

    versiyon 2:fonksiyon yazildi

--- src/Ogrenci_git.java
+++ src/Ogrenci_git.java
@@ -1,5 +1,10 @@
 public class Ogrenci_git {
     public static void main(String[] args) {
-        System.out.println("  projenin ilk asamasi ");
+
+
+        System.out.println("  not girisi bolumu cagirildi ");
+    }
+    public static void ogrenci_not_ekle(){
+        System.out.println("eklemeler");
     }
 }
